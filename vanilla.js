6c55fe3ccc69799b0f3625ce3f2df25ed91b1a35
daniluengo1 <daniel@balinvest.es>
2026-08-03 12:04:18 +0200

    Delete vanilla.js

--- vanilla.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-async function run() {
-  const customApiBaseUrl = `https://api.businesscentral.dynamics.com/v2.0/8f3b1dc3-ce73-455b-9d41-e945c7b9ab88/Production/api/craze/integrations/v1.0`;
-  console.log(customApiBaseUrl);
-}
-run();
