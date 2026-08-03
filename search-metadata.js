f5e628540bae3d6e0344001bb4412ce97850abde
daniluengo1 <daniel@balinvest.es>
2026-08-03 12:02:48 +0200

    Delete search-metadata.js

--- search-metadata.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const xml = fs.readFileSync('metadata.xml', 'utf8');
-const lines = xml.split('<EntityType Name="');
-console.log("Entity Types:");
-for(let i=1; i<lines.length; i++){
-  console.log(lines[i].substring(0, lines[i].indexOf('"')));
-}
