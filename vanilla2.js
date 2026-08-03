1cad3d33037b839d10ea32adda4a04bc0d3eb7e0
daniluengo1 <daniel@balinvest.es>
2026-08-03 12:04:26 +0200

    Delete vanilla2.js

--- vanilla2.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function run() {
-  const config = await prisma.businessCentralConfig.findFirst();
-  const token = await require('./src/lib/bcAuth.ts').getAccessToken(); // Wait, this is TS. We can't require it in Node.
-}
-run();
