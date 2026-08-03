04a386008d6cec56efcd43773648bf7eb6d6d8c6
daniluengo1 <daniel@balinvest.es>
2026-08-03 12:00:55 +0200

    Delete runSync2.ts

--- runSync2.ts
+++
@@ -1,5 +0,0 @@
-import { syncBusinessCentral } from './src/lib/bcSync.ts';
-
-syncBusinessCentral()
-  .then(res => console.log('Sync finished', res))
-  .catch(err => console.error('Sync failed', err));
