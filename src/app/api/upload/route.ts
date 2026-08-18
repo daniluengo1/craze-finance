7a15f3e2ae263c3ac775a7504395780d9227c253
daniluengo1 <daniel@balinvest.es>
2026-08-18 13:44:43 +0200

    Add files via upload

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request): Promise<NextResponse> {
       request,
       onBeforeGenerateToken: async (pathname) => {
         return {
-          allowedContentTypes: ['application/pdf'],
+          allowedContentTypes: ['application/pdf', 'image/jpeg', 'image/png', 'image/webp'],
           tokenPayload: JSON.stringify({}),
         };
       },
