19404d0b1fb17896b9deb3b19a22b3f06a7714e0
daniluengo1 <daniel@balinvest.es>
2026-08-18 13:43:14 +0200

    Delete src/app/api/upload/src directory

--- src/app/api/upload/src/scripts/checkDb.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function main() {
-  console.log(await prisma.purchaseInvoice.findFirst({ where: { bcId: '2173' } }));
-  console.log(await prisma.invoice.findFirst({ where: { confirmedPaymentDate: { not: null } } }));
-}
-main();
